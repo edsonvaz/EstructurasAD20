24bb1ad0ed0b3e9a41e81cfa32804675bd3f4584
Edson Vázquez <e9819@outlook.com>
2020-09-03 05:57:49 +0000

    3_vector test 12...PASSED. SUCCESS!

--- casos/3_vector/vector.h
+++ casos/3_vector/vector.h
@@ -80,12 +80,64 @@ unsigned int Vector<T>::length() const {
 
 template <class T>
 unsigned int Vector<T>::resize(unsigned int newSize) throw (RangeError, OutOfMemory) {
-	return 0;
+	
+ 	if (newSize == 0) {
+		throw RangeError();
+	}
+	T *newData = new int[newSize];
+	if (newData == 0) {
+		throw OutOfMemory();
+	}
+
+	if (newSize <= size) {
+		for (unsigned int i = 0; i < newSize; i++) {
+			newData[i] = data[i];
+		}
+	} else {
+		for (unsigned int i = 0; i < size; i++) {
+			newData[i] = data[i];
+		}
+	}
+
+	delete [] data;
+	data = newData;
+	size = newSize;
+
+	return size;
+
 }
 
 template <class T>
 unsigned int Vector<T>::resize(unsigned int newSize, T &initValue) throw (RangeError, OutOfMemory) {
-	return 0;
+  
+if (newSize == 0) {
+		throw RangeError();
+	}
+	T *newData = new int[newSize];
+	if (newData == 0) {
+		throw OutOfMemory();
+	}
+
+	if (newSize <= size) {
+		for (unsigned int i = 0; i < newSize; i++) {
+			newData[i] = data[i];
+		}
+	} else {
+		unsigned int i;
+		for (i = 0; i < size; i++) {
+			newData[i] = data[i];
+		}
+		for (; i < newSize; i++) {
+			newData[i] = initValue;
+		}
+	}
+
+	delete [] data;
+	data = newData;
+	size = newSize;
+
+	return size;
+
 }
 
 template <class T>
@@ -102,11 +154,20 @@ std::string Vector<T>::toString() const {
 
 template <class T>
 T& Vector<T>::operator[] (unsigned int index) const throw (IndexOutOfBounds) {
-	return data[0];
-}
+if (index < 0 || index >= size) {
+		throw IndexOutOfBounds();
+	}
+	return data[index];
+  }
 
 template <class T>
 void Vector<T>::operator=(const Vector<T> &right) {
+  	if (size != right.size) {
+		resize(right.size);
+	}
+	for (unsigned int i = 0; i < right.size; i++) {
+		data[i] = right.data[i];
+	}
 }
 
 #endif /* VECTOR_H_ */
