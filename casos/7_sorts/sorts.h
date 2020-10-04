b3e123293e7b49930ba4cae6457b7d5c4fcf2fae
Edson Vázquez <e9819@outlook.com>
2020-10-04 19:11:39 +0000

    sort success

--- casos/7_sorts/sorts.h
+++ casos/7_sorts/sorts.h
@@ -78,25 +78,79 @@ return v;
 template <class T>
 std::vector<T> Sorts<T>::insertionSort(const std::vector<T> &source) {
 	std::vector<T> v(source);
+
+	for (int i = 1; i < v.size(); i++) {
+		for (int j = i; j > 0 && v[j] < v[j - 1]; j--) {
+			swap(v, j, j - 1);
+		}
+	}
 	return v;
 }
 
 template <class T>
 std::vector<T> Sorts<T>::shellSort(const std::vector<T> &source) {
 	std::vector<T> v(source);
+	int gap = v.size() / 2;
+
+	while (gap > 0) {
+		for (int i = gap; i < v.size(); i++) {
+			for (int j = i; j >= gap && v[j] < v[j - gap]; j -= gap) {
+				swap(v, j, j - gap);
+			}
+		}
+		gap /= 2;
+	}
 	return v;
 }
 
 template <class T>
 void Sorts<T>::copyArray(std::vector<T> &A, std::vector<T> &B, int low, int high) {
+	for (int i = low; i <= high; i++) {
+		A[i] = B[i];
+	}
 }
 
 template <class T>
 void Sorts<T>::mergeArray(std::vector<T> &A, std::vector<T> &B, int low, int mid, int high) {
+	int i, j, k;
+
+	i = low;
+	j = mid + 1;
+	k = low;
+
+	while (i <= mid &&j <= high) {
+		if (A[i] < A[j]) {
+			B[k] = A[i];
+			i++;
+		} else {
+			B[k] = A[j];
+			j++;
+		}
+		k++;
+	}
+	if (i > mid) {
+		for (; j <= high; j++) {
+			B[k++] = A[j];
+		}
+	} else {
+		for (; i <= mid; i++) {
+			B[k++] = A[i];
+		}
+	}
 }
 
 template <class T>
 void Sorts<T>::mergeSplit(std::vector<T> &A, std::vector<T> &B, int low, int high) {
+	int mid;
+
+	if ( (high - low) < 1 ) {
+		return;
+	}
+	mid = (high + low) / 2;
+	mergeSplit(A, B, low, mid);
+	mergeSplit(A, B, mid + 1, high);
+	mergeArray(A, B, low, mid, high);
+	copyArray(A, B, low, high);
 }
 
 template <class T>
@@ -104,12 +158,12 @@ std::vector<T> Sorts<T>::mergeSort(const std::vector<T> &source) {
 	std::vector<T> v(source);
 	std::vector<T> tmp(v.size());
 
+	mergeSplit(v, tmp, 0, v.size() - 1);
 	return v;
 }
 
 template <class T>
 std::vector<T> Sorts<T>::bucketSort(const std::vector<T> &source) {
-	typename  std::list<T>::iterator itr;
 	std::vector<T> v;
 
 	return v;
